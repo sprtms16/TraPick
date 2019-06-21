7b14fcbd91c8a994ded36d91fbc4d0f628f65b27
parkgwanyong <pgy9394@naver.com>
2019-06-21 14:15:58 +0900

    add search method

--- src/trapick/feed/service/FeedService.java
+++ src/trapick/feed/service/FeedService.java
@@ -1,5 +1,6 @@
 package trapick.feed.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,12 +42,20 @@ public class FeedService {
 		int user_idx = (int) session.getAttribute("user_idx");
 
 		Map<String, Object> map = new HashMap<String, Object>();
+		List<String> keywordList = new ArrayList<String>();
 		map.put("user_idx", user_idx);
-		if (request.getParameter("keyword") != null) {
-			String keyword = request.getParameter("keyword");
-			map.put("keyword", "%#" + keyword + "%");
-			System.out.println(request.getParameter("keyword"));
+		if (request.getParameter("keyword") != null) { // hashtag
+			String keyword = "#" + request.getParameter("keyword");
+			keywordList.add(keyword);
+			map.put("keyword", keywordList);
 		}
+		if (request.getParameter("word") != null) { // 검색창에 입력한 text값
+			String word = request.getParameter("word");
+			for (String words : word.split(" "))
+				keywordList.add(words);
+			map.put("keyword", keywordList);
+		}
+
 		List<Feed> list = dao.feedList(map);
 		return list;
 	}
